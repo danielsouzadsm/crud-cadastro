d76096017d8bf37fc16279f73a510ab9e9c5b446
unknown <danielsouzadsm1@gmail.com>
2025-03-03 17:59:05 -0300

    adicionando db a API e mudanças do mesmo

--- crud.js
+++ crud.js
@@ -1,70 +1,77 @@
 let express = require('express');
 const fs = require('fs');
+const db = require('mysql2')
 const app = express();
 const PORT = 3000;
+
 
 app.use(express.json());
 app.use(express.static('crud-web'));
 
-const arquivo = 'dados.json'
+const ligar = db.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'crud'
+});
 
-function GetUsuarios(){
-    try{
-        if(!fs.existsSync(arquivo)){
-            fs.writeFileSync(arquivo, '[]');
-        }
-        const data = fs.readFileSync(arquivo,'UTF-8')
-        return JSON.parse(data)
+ligar.connect(error => {
+    if (error) {
+        console.error('Erro ao conectar MySQL!', error)
+        return;
+    } else
+        console.error('MySQL conectado!')
+});
 
-    }  catch(error){
-        return [];
-    }
-}
-
-function postUsuarios(usuarios){
-    fs.writeFileSync(arquivo, JSON.stringify(usuarios, null, 2))
-}
- 
-let usuarios = GetUsuarios()
 app.get('/favicon.ico', (req, res) => res.status(204));
+
 app.get('/usuarios', (req, res) => {
-    return res.json(usuarios)
+    ligar.query('SELECT * FROM usuarios', (error, results) => {
+        if (error) {
+            return res.status(500).json({error:'Erro na busca de usuários'})
+        }
+        return res.json(results);
+    });
+
 });
 
 app.post('/usuarios', (req, res) => {
     const { nome, sobrenome, idade } = req.body;
-    usuarios.push({nome, sobrenome, idade})
-    postUsuarios(usuarios)
-
-    return res.json(usuarios);
-});
-
-
-app.put('/usuarios/:index',(req, res) => {
-    const index = parseInt(req.params.index);
-    const {nome, sobrenome, idade} = req.body;
+    const db = 'INSERT INTO usuarios (nome, sobrenome, idade) VALUES(?, ?, ?)';
     
-if(usuarios[index]) {
-    usuarios[index] = {
-        nome: nome ?? usuarios[index].nome,
-        sobrenome: sobrenome ?? usuarios[index].sobrenome,
-        idade: idade ?? usuarios[index].idade
-    };
-    postUsuarios(usuarios);
-    return res.json(usuarios);
-}else
-    return res.status(404).json({ error: "Usuário não encontrado" });
-
-});
-
-app.delete('/usuarios/:index',(req,res) => {
-    const index = parseInt(req.params.index);
-    
-    usuarios.splice(index, 1);
-    postUsuarios(usuarios);
-    return res.json({message: 'O usuario foi deletado!'});
+   ligar.query(db,[nome, sobrenome, idade], (error,result) => {
+    if(error){
+        return res.status(500).json({error:'Erro ao inserir usuário'});
+    }
+    return res.json({id: result.insertId, nome,sobrenome, idade});
+   });
 });
 
 
+app.put('/usuarios/:id', (req, res) => {
+    const {id} = req.params;
+    const { nome, sobrenome, idade } = req.body;
+    const db = 'UPDATE usuarios SET nome = ?, sobrenome = ?, idade = ? WHERE id = ?'
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+    ligar.query(db, [nome, sobrenome, idade, id], (error,result) => {
+        if (error){
+            return res.status(500).json({ error: 'Usuário não atualizado' });
+        }
+        return res.json({message: 'Usuário atualizado!'});
+    });
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    const {id} = req.params;
+    const db = 'DELETE FROM usuarios WHERE id = ?';
+
+    ligar.query(db, [id], (error, result) => {
+        if(error){
+            return res.status(500).json({error: 'Error ao deletar!'})
+        }
+        return res.json({ message: 'O usuario foi deletado!' });
+    })
+
+});
+
+app.listen(PORT, () => console.log(`Servidor online! Porta  ${PORT}`));
